2d907f92ce9fd7f21f9be8c5dabd9c97b2211e4c
Alexandr Kochurov <b2b4ly@gmail.com>
2022-03-31 02:05:07 +0400

    feat: 29g281p удаление лишнего кода для icon

--- src/components/_shared/index.js
+++ src/components/_shared/index.js
@@ -1,13 +1,11 @@
 import imageInit from './image';
 import checkboxInit from './checkbox';
 import fullTooltipInit from './elem-full-tooltip';
-import initIcon from './icon';
 
 const init = () => {
   imageInit();
   checkboxInit();
   fullTooltipInit();
-  initIcon();
 };
 
 export default init;
